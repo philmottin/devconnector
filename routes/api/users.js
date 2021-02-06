6e679cc4856573839237055ac88170ae49842d02
Phil Mottin <philmottin@gmail.com>
2021-02-06 23:27:41 +0000

    Created routes for posts, users and profile

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public
+router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
+
+module.exports = router;
