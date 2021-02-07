29dcd548502fcd62b027756d0424ada2c82e9165
Phil Mottin <philmottin@gmail.com>
2021-02-07 21:48:35 +0000

    Input Validation for login and register methods
    Add global function is-empty() to check if any type is empty

---
+++ validation/login.js
@@ -0,0 +1,26 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  // if data.field is blank, it will not be an empty string, it will be an empty object so our global isEmpty function checks if its empty and return as empty string
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password.toString() : '';
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
