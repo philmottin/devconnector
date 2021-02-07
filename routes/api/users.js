e8514f7dd2b414177a7c7021e7f92f9b54288018
Phil Mottin <philmottin@gmail.com>
2021-02-07 14:30:51 +0000

    Implemented JWT token
    Generate JWT token with login method

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User Model
 const User = require('../../models/User');
@@ -67,7 +69,24 @@ router.post('/login', (req, res) => {
       .compare(password, user.password)
       .then((isMatch) => {
         if (isMatch) {
-          res.json({ msg: 'Sucess' });
+          // User matched
+
+          // Create JWT poyload
+          const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar,
+          };
+
+          // Signed Token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({ sucess: true, token: 'Bearer ' + token });
+            }
+          );
         } else {
           return res.status(400).json({ password: 'Password incorrect' });
         }
