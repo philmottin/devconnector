b1d72c299710309a39a3e3b003bde00d73fa59ab
Phil Mottin <philmottin@gmail.com>
2021-02-12 22:40:38 +0000

    Created Components
    InputGroup, SelectListGroup, TextAreaFieldGroup

---
+++ client/src/components/common/SelectListGroup.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const SelectListGroup = ({ name, value, error, info, onChange, options }) => {
+  const selectOption = options.map((option) => (
+    <option key={option.label} value={option.value}>
+      {option.value}
+    </option>
+  ));
+  return (
+    <div className='form-group'>
+      <select
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error,
+        })}
+        name={name}
+        value={value}
+        onChange={onChange}
+      >
+        {selectOption}
+      </select>
+      {info ? <small className='form-text text-muted'>{info}</small> : ''}
+      {error ? <div className='invalid-feedback'>{error}</div> : ''}
+    </div>
+  );
+};
+
+SelectListGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  optiona: PropTypes.array.isRequired,
+};
+
+export default SelectListGroup;
