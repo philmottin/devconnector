959404cc13fec720e78ca7e17371f398a9297a86
Phil Mottin <philmottin@gmail.com>
2021-02-13 17:50:01 +0000

    Add deleteAccount on Action Profile
    Create ProfileActions componentes inside Dashboard Component

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,10 @@ import {
   PROFILE_LOADING,
   GET_ERRORS,
   CLEAR_CURRENT_PROFILE,
+  SET_CURRENT_USER,
 } from './types';
+
+import { logoutUser } from './authAction';
 
 // Get current profile
 export const getCurrentProfile = () => (dispatch) => {
@@ -25,7 +28,7 @@ export const getCurrentProfile = () => (dispatch) => {
     );
 };
 
-// Create Profule
+// Create Profile
 export const createProfile = (profileData, history) => (dispatch) => {
   axios
     .post('/api/profile', profileData)
@@ -36,6 +39,31 @@ export const createProfile = (profileData, history) => (dispatch) => {
         payload: err.response.data,
       })
     );
+};
+
+// Delete account and Profile
+export const deleteAccount = () => (dispatch) => {
+  if (window.confirm('Are you sure? this can not be undone!')) {
+    axios
+      .delete('/api/profile')
+      .then(
+        (res) => {
+          dispatch(logoutUser());
+          dispatch(clearCurrentProfile());
+        }
+
+        //dispatch({
+        //  type: SET_CURRENT_USER,
+        //  payload: {},
+        //})
+      )
+      .catch((err) =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data,
+        })
+      );
+  }
 };
 
 // Profile loading
