1e021ba0741bb96627ad31826d0a8cfbae9aad8c
Phil Mottin <philmottin@gmail.com>
2021-02-14 02:24:40 +0000

    Display Dashboard Components
    Created Education and Experience Components
    Add Actions to Delete experience and education
    Installed packages in CLIENT react-moment, moment
    Add back button to EditProfile

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import { deleteEducation } from '../../actions/profileActions';
+
+class Education extends Component {
+  onDeleteClick(id) {
+    this.props.deleteEducation(id);
+  }
+
+  render() {
+    const education = this.props.education.map((edu) => (
+      <tr key={edu._id}>
+        <td>{edu.school}</td>
+        <td>{edu.degree}</td>
+        <td>
+          <Moment format='YYYY/MM//DD'>{edu.from}</Moment> -{' '}
+          {edu.to === null ? (
+            'Now'
+          ) : (
+            <Moment format='YYYY/MM//DD'>{edu.to}</Moment>
+          )}
+        </td>
+        <td>
+          <button
+            onClick={this.onDeleteClick.bind(this, edu._id)}
+            className='btn btn-danger'
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ));
+    return (
+      <div>
+        <h4 className='mb-2'>Education Credentials</h4>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>School</th>
+              <th>Degree</th>
+              <th>Year</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>{education}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+Education.propTypes = {
+  deleteEducation: PropTypes.func.isRequired,
+  //deleteAccount: PropTypes.func.isRequired,
+  //auth: PropTypes.object.isRequired,
+  //profile: PropTypes.object.isRequired,
+};
+
+//const mapStateToProps = (state) => ({
+//auth: state.auth,
+//profile: state.profile,
+//});
+
+export default connect(null, { deleteEducation })(Education);
--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
@@ -188,6 +188,9 @@ class EditProfile extends Component {
         <div className='container'>
           <div className='row'>
             <div className='col-md-8 m-auto'>
+              <Link to='/dashboard' className='btn btn-light'>
+                Go back
+              </Link>
               <h1 className='display-4 text-center'>Edit profile</h1>
               <small className='d-block pb-3'>* = required fields</small>
               <form onSubmit={this.onSubmit}>
