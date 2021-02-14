abec254ecd719c229435fa0c5483118cc1132a75
Phil Mottin <philmottin@gmail.com>
2021-02-14 19:17:57 +0000

    Post And PostForm Components
    Add postReducer for ADD_POST
    Add Link in Navbar to Post Feed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -16,12 +16,21 @@ class Navbar extends Component {
     const authLinks = (
       <ul className='navbar-nav ml-auto'>
         <li className='nav-item'>
+          <Link className='nav-link' to='/feed'>
+            Post Feed
+          </Link>
+        </li>
+        <li className='nav-item'>
           <Link className='nav-link' to='/dashboard'>
             Dashboard
           </Link>
         </li>
         <li className='nav-item'>
-          <a href='#' onClick={this.onLogoutClick.bind(this)}>
+          <a
+            href='#'
+            onClick={this.onLogoutClick.bind(this)}
+            className='nav-link'
+          >
             <img
               className='rounded-circle'
               src={user.avatar}
