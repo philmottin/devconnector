118c7917d8ec123b5327b9974412d7cc21753a6d
Phil Mottin <philmottin@gmail.com>
2021-02-16 00:27:47 +0000

    Heroku redirect http to https

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ const app = express();
 // Body parser
 app.use(express.json());
 
+// Heroku redirect http to https
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else next();
+  });
+}
+
 // DB Config
 const db = require('./config/keys').mongoURI;
 
