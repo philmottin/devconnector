e39bf1c4e7c61fa15bd345a77d059ba19d314b42
Phil Mottin <philmottin@gmail.com>
2021-02-16 00:58:31 +0000

    FIx redux store with chrome devtools
    Redux store only load redux devtools in dev mode

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,25 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+let store;
+
+// if in production run the store without chrome redux devtools extension
+if (process.env.NODE_ENV === 'production') {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+}
 
 export default store;
